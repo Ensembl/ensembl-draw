de5a5aa34c8749f6ec55521787192c6dee073815
Web Admin <w3adm@sanger.ac.uk>
2001-05-11 10:33:54 +0000

    new png thing on branch

--- modules/Bio/EnsEMBL/Renderer/png.pm
+++ modules/Bio/EnsEMBL/Renderer/png.pm
@@ -1,8 +1,10 @@
-package Bio::EnsEMBL::Renderer::png;
+package Bio::EnsEMBL::Renderer::pnt;
 use strict;
 use lib ".";
 use Bio::EnsEMBL::Renderer::gif;
 use vars qw(@ISA);
+use lib "../../../../../modules";
+use WMF;
 @ISA = qw(Bio::EnsEMBL::Renderer::gif);
 
 sub canvas {
