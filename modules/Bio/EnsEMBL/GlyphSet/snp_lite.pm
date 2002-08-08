4589de5d3437c05e2aa320b51b389404318cd295
Graham McVicker <mcvicker@sanger.ac.uk>
2002-08-08 11:00:05 +0000

    Updated snp_lite for new core code - now uses ExternalData::Variation object for drawing instead of anonymous feature hash

--- modules/Bio/EnsEMBL/GlyphSet/snp_lite.pm
+++ modules/Bio/EnsEMBL/GlyphSet/snp_lite.pm
@@ -7,53 +7,85 @@ use Bio::EnsEMBL::GlyphSet_simple_hash;
 sub my_label { return "SNPs"; }
 
 sub features {
-    my ($self) = @_;
-    my $glob;
-    $glob = $self->glob_bp() unless $self->{'config'}->get( $self->check(), 'dep' )>1;
-    my @snps = sort { $a->{'type'} cmp $b->{'type'} || $a->{'chr_start'} <=> $b->{'chr_start'} } @{$self->{'container'}->get_all_SNPFeatures_lite( $glob ) };
-    if(@snps) {
-        $self->{'config'}->{'snp_legend_features'}->{'snps'} = { 'priority' => 1000, 'legend' => [] }
-    }
-    return @snps;
+  my ($self) = @_;
+  
+  my @snps = sort { $a->type() cmp $b->type() || $a->start() <=> $b->start()}  
+                  $self->{'container'}->get_all_SNPs();
+
+  if(@snps) {
+    $self->{'config'}->{'snp_legend_features'}->{'snps'} 
+        = { 'priority' => 1000, 'legend' => [] };
+  }
+
+  return @snps;
 }
 
 sub href {
     my ($self, $f ) = @_;
-    return "/$ENV{'ENSEMBL_SPECIES'}/snpview?snp=$f->{'id'}&chr=$f->{'chr_name'}&vc_start=$f->{'chr_start'}";
+    
+    my $snp_id = $f->id();
+    my $chr_start = $self->{'container'}->chr_start();
+    my $chr_name = $self->{'container'}->chr_name();
+
+    return "/$ENV{'ENSEMBL_SPECIES'}/snpview?snp=$snp_id&chr=$chr_name&vc_start=$chr_start";
 }
 
 sub colour {
-    my ($self, $f) = @_;
-    my $T = substr($f->{'type'},3);
-    unless($self->{'config'}->{'snp_types'}{$T}) {
-        my %labels = (
-            '_coding' => 'Coding SNPs',
-            '_utr'    => 'UTR SNPs',
-            '_intron' => 'Intronic SNPs',
-            '_local'  => 'Flanking SNPs',
-            '_'       => 'other SNPs'
-        );
-        push @{ $self->{'config'}->{'snp_legend_features'}->{'snps'}->{'legend'} }, $labels{"_$T"} => $self->{'colours'}{"_$T"};
-        $self->{'config'}->{'snp_types'}{$T}=1;
-    }
-    return $self->{'colours'}{"_$T"};
+  my ($self, $f) = @_;
+
+  my $type = substr($f->type(), 3);
+
+  unless($self->{'config'}->{'snp_types'}{$type}) {
+    my %labels = (
+	 '_coding' => 'Coding SNPs',
+	 '_utr'    => 'UTR SNPs',
+	 '_intron' => 'Intronic SNPs',
+	 '_local'  => 'Flanking SNPs',
+	 '_'       => 'Other SNPs' );
+    push @{ $self->{'config'}->{'snp_legend_features'}->{'snps'}->{'legend'}},
+           $labels{"_$type"} => $self->{'colours'}{"_$type"};
+    $self->{'config'}->{'snp_types'}{$type} = 1;
+  }
+
+  return $self->{'colours'}{"_$type"};
 }
+
 
 sub zmenu {
     my ($self, $f ) = @_;
     my $ext_url = $self->{'config'}->{'ext_url'};
     
+    my $chr_start = $f->start() + $self->{'container'}->chr_start() - 1;
+
     my %zmenu = ( 
-        'caption'           => "SNP: ".$f->{'id'},
+        'caption'           => "SNP: ".$f->id(),
         '01:SNP properties' => $self->href( $f ),
-        "02:bp: $f->{'chr_start'}" => '',
-        '03:dbSNP data'     => $ext_url->get_url('SNP', $f->{'id'}),
-    );
-    $zmenu{"04:TSC-CSHL data"} = $ext_url->get_url( 'TSC-CSHL', $f->{'tscid'} )    if defined $f->{'tscid'};
-#    $zmenu{"06:CGAP-GAI data"} = $ext_url->get_url( 'CGAP-GAI', $pid );  
-    $zmenu{"05:HGBASE data"}   = $ext_url->get_url( 'HGBASE',   $f->{'hgbaseid'} ) if defined $f->{'hgbaseid'};  
-    my $T = substr($f->{'type'},3);
-    $zmenu{"06:Type: $T"}   = "" unless $T eq '';  
+        "02:bp: $chr_start" => '',
+     
+   );
+
+    if($f->id() > 0) {
+      $zmenu{'03:dbSNP data'} = $ext_url->get_url('SNP', $f->id());
+    }
+
+    my %links;
+    
+    foreach my $link ($f->each_DBLink()) {
+      $links{$link->database()} = $link->primary_id();
+    }
+
+    if(defined $links{'TSC-CSHL'}) {
+      $zmenu{"04:TSC-CSHL data"} 
+         = $ext_url->get_url( 'TSC-CSHL', $links{'TSC-CSHL'} );      
+    }
+
+    if(defined $links{'HGBASE'}) {
+      $zmenu{"05:HGBASE data"}  
+         = $ext_url->get_url( 'HGBASE', $links{'HGBASE'});
+    }
+    
+    my $type = substr($f->type(),3);
+    $zmenu{"06:Type: $type"} = "" unless $type eq '';  
     return \%zmenu;
 }
 1;
