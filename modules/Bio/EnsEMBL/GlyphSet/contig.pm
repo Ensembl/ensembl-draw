94239a545129d6e777bdbb47dc7258aa76fe41ed
Graham McVicker <mcvicker@sanger.ac.uk>
2002-08-21 12:04:22 +0000

    contig tiling path now draws in human and mouse assemblies

--- modules/Bio/EnsEMBL/GlyphSet/contig.pm
+++ modules/Bio/EnsEMBL/GlyphSet/contig.pm
@@ -62,74 +62,84 @@ sub _init {
   my @contig_tiling_path = $vc->get_tiling_path();
   
   my $useAssembly;
-  eval {
-    # Do we have assembly_contigs?
-    $useAssembly = $vc->has_MapSet( 'assembly' ); 
-  };
+
+  # Do we have assembly_contigs?
+  $useAssembly = $vc->has_MapSet( 'assembly' ); 
     
   if (!@contig_tiling_path) {
     ## Draw a warning track....
     $self->errorTrack("Golden path gap - no contigs to display!");
   } elsif($useAssembly && $length < MAX_VIEWABLE_ASSEMBLY_SIZE) {
-    $self->_init_assembled_contig($ystart, \@contig_tiling_path);
+      $self->_init_assembled_contig($ystart, \@contig_tiling_path);
   } else {
     $self->_init_non_assembled_contig($ystart, \@contig_tiling_path);
   }
 }
 
-
-
+#
+# Draws contig tiling path for mouse
+#
 sub _init_assembled_contig {
-  my ($self, $ystart, $map_contigs) = @_;
-  my @map_contigs;
+  my ($self, $ystart, $contig_tiling_path) = @_;
 
   my $vc = $self->{'container'};
+  
   my $length = $vc->length() +1;
-  my $Config = $self->{'config'};
 
+  my $Config = $self->{'config'};
 
   my $module = ref($self);
   $module = $1 if $module=~/::([^:]+)$/;
   my $threshold_navigation = ($Config->get($module, 'threshold_navigation') 
 			      || 2e6)*1001;
   my $navigation     = $Config->get($module, 'navigation') || 'on';
-  my $show_navigation = ($length < $threshold_navigation) && ($navigation eq 'on');
+  my $show_navigation = 
+    ($length < $threshold_navigation) && ($navigation eq 'on');
 
   my $cmap     = $Config->colourmap();
-  my $black    = $cmap->id_by_name('black');
-  my $green    = $cmap->id_by_name('green');
-  my $im_width = $Config->image_width();
-
   my ($w,$h)   = $Config->texthelper()->real_px2bp('Tiny');
+
+########
+# Vars used only for scale drawing
+#
+  my $black    = $cmap->id_by_name('black');
+  my $red      = $cmap->id_by_name('red');
+  my $highlights = join('|', $self->highlights());
+  $highlights = $highlights ? "&highlight=$highlights" : '';
   my $clone_based = $Config->get('_settings','clone_based') eq 'yes';
-  my $param_string   = $clone_based ? $Config->get('_settings','clone') : ("chr=".$vc->chr_name());
-  my $global_start   = $clone_based ? $Config->get('_settings','clone_start') : $vc->chr_start();
+  my $param_string   = $clone_based ? 
+    $Config->get('_settings','clone') : ("chr=".$vc->chr_name());
+  my $global_start   = $clone_based ? 
+    $Config->get('_settings','clone_start') : $vc->chr_start();
   my $global_end     = $global_start + $length - 1;
+  my $im_width = $Config->image_width();
+#
+########
 
-
+########
+# Draw the contig tiling path
+#
   my $i = 1;
-    
   my %colours  = ( $i  => $cmap->id_by_name('contigblue1'), 
 		     !$i => $cmap->id_by_name('contigblue2'));
   my %colours2 = ( $i  => $cmap->id_by_name('grey2'), 
 		   !$i => $cmap->id_by_name('grey3'));
-
     
   $w *= $length/($length-1);
 
   my @assembly_contigs = $vc->get_all_MapFrags( 'assembly' );
   my %contigs = ();
   my %big_contigs = ();
-  foreach my $big_contig ( @map_contigs ) {
-    my $ID = $big_contig->contig->id();            
+  foreach my $tile ( @{$contig_tiling_path} ) {
+    my $ID = $tile->{'contig'}->name();            
     $contigs{ $ID } = [];
-    $big_contigs{ $ID } = [ $big_contig->start, $big_contig->end ];
+    $big_contigs{ $ID } = [ $tile->{'start'}, $tile->{'end'} ];
     foreach my $little_contig (@assembly_contigs) {
       my $start = $little_contig->start;
       my $end   = $little_contig->end;
-      if( $end   >= $big_contig->start || $start <= $big_contig->end ) {
-	$start = $big_contig->start if $big_contig->start > $start;
-	$end   = $big_contig->end   if $big_contig->end   < $end;
+      if( $end   >= $tile->{'start'} || $start <= $tile->{'end'} ) {
+	$start = $tile->{'start'} if $tile->{'start'} > $start;
+	$end   = $tile->{'end'}   if $tile->{'end'}   < $end;
 	push @{$contigs{$ID}}, [ $start, $end ]
       }
     }
@@ -161,7 +171,7 @@ sub _init_assembled_contig {
                 'y'         => $ystart-3,
                 'width'     => 1,
                 'height'    => 21,
-                'colour'    => $green,
+                'colour'    => $cmap->id_by_name('green'),
                 'absolutey' => 1,
             }); 
     }
@@ -185,146 +195,14 @@ sub _init_assembled_contig {
      
 
     my $bp_textwidth = $w * length($_) * 1.2; # add 20% for scaling text
-    #unless (1||$bp_textwidth > $composite->width() ) {
-#      my $tglyph = new Bio::EnsEMBL::Glyph::Text({
-#                    'x'          => int( (2 * $composite->x() + $composite->width() - $bp_textwidth)/2),
-#                    'y'          => $ystart+5,
-#                    'font'       => 'Tiny',
-#                    'colour'     => $black,
-#                    'text'       => $_,
-#                    'absolutey'  => 1,
-#                });
 
-#      my $glyph = new Bio::EnsEMBL::Glyph::Rect({
-#                    'x'          => int( (2 * $composite->x() + $composite->width() - $bp_textwidth)/2)-0.2/$w,
-#                    'y'          => $ystart+5,
-#                    'width'      => $bp_textwidth,
-#                    'height'     => 7,
-#                    'colour'     => $cmap->id_by_name('white'),
-#                    'text'       => $_,
-#                    'absolutey'  => 1,
-#                });
-         
-#      $composite->push($glyph);
-#      $composite->push($tglyph);
-#    }
     $self->push($composite);
   }
-}
- 
 
-
-
-
-
-
-sub _init_non_assembled_contig {
-  my ($self, $ystart, $contig_tiling_path) = @_;
-
-  my $gline;
-
-  my $vc = $self->{'container'};
-  my $length = $vc->length() +1;
-  my $Config = $self->{'config'};
-
-  my $module = ref($self);
-  $module = $1 if $module=~/::([^:]+)$/;
-  my $threshold_navigation = ($Config->get($module, 'threshold_navigation') 
-			      || 2e6)*1001;
-  my $navigation     = $Config->get($module, 'navigation') || 'on';
-  my $show_navigation = ($length < $threshold_navigation) && ($navigation eq 'on');
-
-  my $highlights = join('|', $self->highlights() ) ;
-  $highlights = $highlights ? "&highlight=$highlights" : '';
-  my $cmap     = $Config->colourmap();
-  
-  my $black    = $cmap->id_by_name('black');
-  my $red      = $cmap->id_by_name('red');
-  my $im_width = $Config->image_width();
-  my ($w,$h)   = $Config->texthelper()->real_px2bp('Tiny');
-  my $clone_based = $Config->get('_settings','clone_based') eq 'yes';
-  my $param_string   = $clone_based ? $Config->get('_settings','clone') : ("chr=".$vc->chr_name());
-  my $global_start   = $clone_based ? $Config->get('_settings','clone_start') : $vc->chr_start();
-  my $global_end     = $global_start + $length - 1;
-    
-  my $i = 1;
-    
-  my %colours  = ( $i  => $cmap->id_by_name('contigblue1'), 
-		   !$i => $cmap->id_by_name('contigblue2'));
-  
-  $w *= $length/($length-1);
-
-  my $tot_width = @$contig_tiling_path[-1]->{'end'} - 
-    @$contig_tiling_path[0]->{'start'} + 1;
-
-  foreach my $tile ( @$contig_tiling_path ) {
-    my $col = $colours{$i};
-    $i      = !$i;
-    
-    my $rend   = $tile->{'end'};
-    my $rstart = $tile->{'start'};
-    my $rid    = $tile->{'contig'}->name();
-    my $clone  = $tile->{'contig'}->clone->embl_id();
-    my $strand = $tile->{'strand'};
-    $rstart     = 1 if $rstart < 1;
-    $rend       = $length if $rend > $length;
-            
-    my $glyph = new Bio::EnsEMBL::Glyph::Rect({
-		'x'         => $rstart,
-		'y'         => $ystart+2,
-		'width'     => $rend - $rstart+1,
-                'height'    => 11,
-                'colour'    => $col,
-                'absolutey' => 1,
-		});
-
-    if($navigation eq 'on') {
-      $glyph->{'href'} = 
-	"/$ENV{'ENSEMBL_SPECIES'}/$ENV{'ENSEMBL_SCRIPT'}?contig=$rid";
-    }
-
-    if($show_navigation) {
-      $glyph->{'zmenu'} = {
-		       'caption' => $rid,
-		       "01:Clone: $clone"    => '',
-		       '02:Centre on contig' => $glyph->{'href'},
-		       "03:EMBL source file" => 
-		       $self->{'config'}->{'ext_url'}->get_url( 'EMBL', $clone)
-		     };
-    }
-
-    $self->push($glyph);
-    $clone = $strand > 0 ? "$clone >" : "< $clone";
-    my $bp_textwidth = $w * length($clone) * 1.2; # add 20% for scaling text
-    
-    if($bp_textwidth > ($rend - $rstart)) {
-      my $pointer = $strand > 0 ? ">" : "<";
-      $bp_textwidth = $w * length($pointer) * 1.2; # add 20% for scaling text
-      unless($bp_textwidth > ($rend - $rstart)){
-	my $tglyph = new Bio::EnsEMBL::Glyph::Text({
-                     'x'          => int( ($rend + $rstart - $bp_textwidth)/2),
-                     'y'          => $ystart+4,
-                     'font'       => 'Tiny',
-                     'colour'     => $cmap->id_by_name('white'),
-                     'text'       => $pointer,
-                     'absolutey'  => 1,
-                    });
-	$self->push($tglyph);
-      }
-    } else {
-      my $tglyph = new Bio::EnsEMBL::Glyph::Text({
-                    'x'          => int( ($rend + $rstart - $bp_textwidth)/2),
-                    'y'          => $ystart+5,
-                    'font'       => 'Tiny',
-                    'colour'     => $cmap->id_by_name('white'),
-                    'text'       => $clone,
-                        'absolutey'  => 1,
-                });
-      $self->push($tglyph);
-    }
-  }
-
-  $gline = new Bio::EnsEMBL::Glyph::Rect({
+######
+# Draw the scale and ticks and red box etc..
+#
+  my $gline = new Bio::EnsEMBL::Glyph::Rect({
         'x'         => 0,
         'y'         => $ystart,
         'width'     => $im_width,
@@ -477,5 +355,282 @@ sub _init_non_assembled_contig {
     }
   }
 }
+ 
+
+#
+# Draws tiling path of contigs for human
+#
+sub _init_non_assembled_contig {
+  my ($self, $ystart, $contig_tiling_path) = @_;
+
+  my $vc = $self->{'container'};
+  my $length = $vc->length() +1;
+
+  my $Config = $self->{'config'};
+
+  my $module = ref($self);
+  $module = $1 if $module=~/::([^:]+)$/;
+  my $threshold_navigation = ($Config->get($module, 'threshold_navigation') 
+			      || 2e6)*1001;
+  my $navigation     = $Config->get($module, 'navigation') || 'on';
+  my $show_navigation = ($length < $threshold_navigation) && ($navigation eq 'on');
+
+  my $cmap     = $Config->colourmap();
+ 
+########
+# Vars used only for scale drawing
+#
+  my $black    = $cmap->id_by_name('black');
+  my $red      = $cmap->id_by_name('red');
+  my $highlights = join('|', $self->highlights());
+  $highlights = $highlights ? "&highlight=$highlights" : '';
+  my $clone_based = $Config->get('_settings','clone_based') eq 'yes';
+  my $param_string   = $clone_based ? 
+    $Config->get('_settings','clone') : ("chr=". $vc->chr_name());
+  my $global_start   = $clone_based ? 
+    $Config->get('_settings','clone_start') : $vc->chr_start();
+  my $global_end     = $global_start + $length - 1;
+  my $im_width = $Config->image_width();
+#
+########
+
+#######
+# Draw the Contig Tiling Path
+#
+  my ($w,$h)   = $Config->texthelper()->real_px2bp('Tiny');    
+  my $i = 1;
+  my %colours  = ( $i  => $cmap->id_by_name('contigblue1'), 
+		   !$i => $cmap->id_by_name('contigblue2'));
+  
+  $w *= $length/($length-1);
+
+  my $tot_width = @$contig_tiling_path[-1]->{'end'} - 
+    @$contig_tiling_path[0]->{'start'} + 1;
+
+  foreach my $tile ( @$contig_tiling_path ) {
+    my $col = $colours{$i};
+    $i      = !$i;
+    
+    my $rend   = $tile->{'end'};
+    my $rstart = $tile->{'start'};
+    my $rid    = $tile->{'contig'}->name();
+    my $clone  = $tile->{'contig'}->clone->embl_id();
+    my $strand = $tile->{'strand'};
+    $rstart     = 1 if $rstart < 1;
+    $rend       = $length if $rend > $length;
+            
+    my $glyph = new Bio::EnsEMBL::Glyph::Rect({
+		'x'         => $rstart,
+		'y'         => $ystart+2,
+		'width'     => $rend - $rstart+1,
+                'height'    => 11,
+                'colour'    => $col,
+                'absolutey' => 1,
+		});
+
+    if($navigation eq 'on') {
+      $glyph->{'href'} = 
+	"/$ENV{'ENSEMBL_SPECIES'}/$ENV{'ENSEMBL_SCRIPT'}?contig=$rid";
+    }
+
+    if($show_navigation) {
+      $glyph->{'zmenu'} = {
+		       'caption' => $rid,
+		       "01:Clone: $clone"    => '',
+		       '02:Centre on contig' => $glyph->{'href'},
+		       "03:EMBL source file" => 
+		       $self->{'config'}->{'ext_url'}->get_url( 'EMBL', $clone)
+		     };
+    }
+
+    $self->push($glyph);
+    $clone = $strand > 0 ? "$clone >" : "< $clone";
+    my $bp_textwidth = $w * length($clone) * 1.2; # add 20% for scaling text
+    
+    if($bp_textwidth > ($rend - $rstart)) {
+      my $pointer = $strand > 0 ? ">" : "<";
+      $bp_textwidth = $w * length($pointer) * 1.2; # add 20% for scaling text
+      unless($bp_textwidth > ($rend - $rstart)){
+	my $tglyph = new Bio::EnsEMBL::Glyph::Text({
+                     'x'          => int( ($rend + $rstart - $bp_textwidth)/2),
+                     'y'          => $ystart+4,
+                     'font'       => 'Tiny',
+                     'colour'     => $cmap->id_by_name('white'),
+                     'text'       => $pointer,
+                     'absolutey'  => 1,
+                    });
+	$self->push($tglyph);
+      }
+    } else {
+      my $tglyph = new Bio::EnsEMBL::Glyph::Text({
+                    'x'          => int( ($rend + $rstart - $bp_textwidth)/2),
+                    'y'          => $ystart+5,
+                    'font'       => 'Tiny',
+                    'colour'     => $cmap->id_by_name('white'),
+                    'text'       => $clone,
+                        'absolutey'  => 1,
+                });
+      $self->push($tglyph);
+    }
+  }
+
+######
+# Draw the scale, ticks, red box etc
+#
+  my $gline = new Bio::EnsEMBL::Glyph::Rect({
+        'x'         => 0,
+        'y'         => $ystart,
+        'width'     => $im_width,
+        'height'    => 0,
+        'colour'    => $black,
+        'absolutey' => 1,
+        'absolutex' => 1,
+    });
+  $self->unshift($gline);
+    
+  $gline = new Bio::EnsEMBL::Glyph::Rect({
+        'x'         => 0,
+        'y'         => $ystart+15,
+        'width'     => $im_width,
+        'height'    => 0,
+        'colour'    => $black,
+        'absolutey' => 1,
+        'absolutex' => 1,    
+    });
+  $self->unshift($gline);
+    
+  ## pull in our subclassed methods if necessary
+  if ($self->can('add_arrows')){
+    $self->add_arrows($im_width, $black, $ystart);
+  }
+
+  my $tick;
+  my $interval = int($im_width/10);
+  for (my $i=1; $i <=9; $i++){
+    my $pos = $i * $interval;
+   
+    # the forward strand ticks
+    $tick = new Bio::EnsEMBL::Glyph::Rect({
+            'x'         => 0 + $pos,
+            'y'         => $ystart-4,
+            'width'     => 0,
+            'height'    => 3,
+            'colour'    => $black,
+            'absolutey' => 1,
+            'absolutex' => 1,
+        });
+    $self->unshift($tick);
+    
+    # the reverse strand ticks
+    $tick = new Bio::EnsEMBL::Glyph::Rect({
+            'x'         => $im_width - $pos,
+            'y'         => $ystart+16,
+            'width'     => 0,
+            'height'    => 3,
+            'colour'    => $black,
+            'absolutey' => 1,
+            'absolutex' => 1,
+        });
+    $self->unshift($tick);
+  }
+    
+  # The end ticks
+  $tick = new Bio::EnsEMBL::Glyph::Rect({
+        'x'         => 0,
+        'y'         => $ystart-2,
+        'width'     => 0,
+        'height'    => 1,
+        'colour'    => $black,
+        'absolutey' => 1,
+        'absolutex' => 1,
+    });
+  $self->unshift($tick);
+   
+  # the reverse strand ticks
+  $tick = new Bio::EnsEMBL::Glyph::Rect({
+        'x'         => $im_width - 1,
+        'y'         => $ystart+16,
+        'width'     => 0,
+        'height'    => 1,
+        'colour'    => $black,
+        'absolutey' => 1,
+        'absolutex' => 1,
+    });
+  $self->unshift($tick);
+    
+  my $vc_size_limit = $Config->get('_settings', 'default_vc_size');
+    # only draw a red box if we are in contigview top and there is a 
+    # detailed display
+    if ($Config->get('_settings','draw_red_box') eq 'yes') { 
+      # only draw focus box on the correct display...
+      my $LEFT_HS = $clone_based ? 0 : $global_start -1;
+      my $boxglyph = new Bio::EnsEMBL::Glyph::Rect({
+            'x'            => $Config->{'_wvc_start'} - $LEFT_HS,
+            'y'            => $ystart - 4 ,
+            'width'        => $Config->{'_wvc_end'} - $Config->{'_wvc_start'},
+            'height'       => 23,
+            'bordercolour' => $red,
+            'absolutey'    => 1,
+        });
+      
+      $self->unshift($boxglyph);
+
+      my $boxglyph2 = new Bio::EnsEMBL::Glyph::Rect({
+            'x'            => $Config->{'_wvc_start'} - $LEFT_HS,
+            'y'            => $ystart - 3 ,
+            'width'        => $Config->{'_wvc_end'} - $Config->{'_wvc_start'},
+            'height'       => 21,
+            'bordercolour' => $red,
+            'absolutey'    => 1,
+        });
+      $self->unshift($boxglyph2);
+    }
+
+  my $width = $interval * ($length / $im_width) ;
+  my $interval_middle = $width/2;
+  
+  if($navigation eq 'on') {
+    foreach my $i(0..9){
+      my $pos = $i * $interval;
+      
+      # the forward strand ticks
+      $tick = new Bio::EnsEMBL::Glyph::Space({
+                'x'         => 0 + $pos,
+                'y'         => $ystart-4,
+                'width'     => $interval,
+                'height'    => 3,
+                'absolutey' => 1,
+                'absolutex' => 1,
+                'href'	    => $self->zoom_URL($param_string, 
+					    $interval_middle + $global_start, 
+					    $length,  1  , $highlights),
+                'zmenu'     => $self->zoom_zmenu($param_string, 
+					    $interval_middle + $global_start, 
+					    $length, $highlights ),
+            });
+      $self->unshift($tick);
+            
+      # the reverse strand ticks
+      $tick = new Bio::EnsEMBL::Glyph::Space({
+                'x'         => $im_width - $pos,
+                'y'         => $ystart+16,
+                'width'     => $interval,
+                'height'    => 3,
+                'absolutey' => 1,
+                'absolutex' => 1,
+                'href'	    => $self->zoom_URL($param_string, 
+					     $global_end-$interval_middle, 
+					     $length,  1  , $highlights),
+                'zmenu'     => $self->zoom_zmenu($param_string, 
+					     $global_end-$interval_middle, 
+					     $length, $highlights ),
+            });
+      $self->unshift($tick);
+      $interval_middle += $width;
+    }
+  }
+}
+
+
 
 1;
