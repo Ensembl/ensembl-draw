12666f404cf07cb68e9b5ee775e6f04fdaf44d95
Web Admin <w3adm@sanger.ac.uk>
2002-09-17 17:18:55 +0000

    fixed bump button bug

--- modules/Bio/EnsEMBL/Renderer/imagemap.pm
+++ modules/Bio/EnsEMBL/Renderer/imagemap.pm
@@ -38,7 +38,7 @@ sub render_Rect {
     $y2 += 1;
     $x2 += 1;
 
-    $self->{'canvas'} = qq(<area coords="$x1 $y1 $x2 $y2"$href>\n).$self->{'canvas'} if(defined $href);
+    $self->{'canvas'} = qq(<area coords="$x1 $y1 $x2 $y2"$href>\n).$self->{'canvas'};
 
 }
 
