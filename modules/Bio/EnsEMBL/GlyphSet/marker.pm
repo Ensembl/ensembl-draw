ea0d53c3c90cb416059fe12ffa68d47238451a19
Web Admin <w3adm@sanger.ac.uk>
2003-02-19 16:06:30 +0000

    Markers from the head, to work with the new head marker schema

--- modules/Bio/EnsEMBL/GlyphSet/marker.pm
+++ modules/Bio/EnsEMBL/GlyphSet/marker.pm
@@ -4,22 +4,49 @@ use vars qw(@ISA);
 use Bio::EnsEMBL::GlyphSet_simple;
 @ISA = qw(Bio::EnsEMBL::GlyphSet_simple);
 
+my $PRIORITY   = 50;
+my $MAP_WEIGHT = 2;
+
 sub my_label { return "Markers"; }
 
 sub features {
     my ($self) = @_;
-    return $self->{'container'}->get_all_landmark_MarkerFeatures();
+
+    return $self->{'container'}->get_all_MarkerFeatures(undef, 
+							$PRIORITY,
+							$MAP_WEIGHT);
 }
 
 sub href {
     my ($self, $f ) = @_;
-    return "/$ENV{'ENSEMBL_SPECIES'}/markerview?marker=".$f->display_label;
+    return "/$ENV{'ENSEMBL_SPECIES'}/markerview?marker=".
+      $f->marker->display_MarkerSynonym->name;
 }
 sub zmenu {
     my ($self, $f ) = @_;
+
+    my $ms = $f->marker->display_MarkerSynonym;
+    my $name = $ms->name;
+    my $src = $ms->source;
+    if($src && $src eq 'unists') {
+      $name = "uniSTS:$name";
+    }
+
     return { 
-        'caption' => $f->display_label,
-	    'Marker info' => $self->href($f)
+        'caption' => $name,
+	 'Marker info' => $self->href($f)
     };
 }
+
+
+sub colour {
+    my ($self, $f) = @_;
+
+    my $type = $f->marker->type;
+
+    $type = '' unless(defined($type));
+
+    return( $self->{'colours'}{"$type"}, $self->{'colours'}{"$type"}, '' );
+}
+
 1;
