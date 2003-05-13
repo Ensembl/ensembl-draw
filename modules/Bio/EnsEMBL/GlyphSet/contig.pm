4a08e5a87cdd33908fc10ce7da7b46cef9a90759
Web Admin <w3adm@sanger.ac.uk>
2003-05-13 13:24:55 +0000

    No EMBL link if no embl_id

--- modules/Bio/EnsEMBL/GlyphSet/contig.pm
+++ modules/Bio/EnsEMBL/GlyphSet/contig.pm
@@ -403,7 +403,7 @@ sub _init_non_assembled_contig {
         my $rstart = $tile->{'start'};
         my $rid    = $tile->{'contig'}->name();
         my $clone  = $tile->{'contig'}->clone->embl_id();
-           $clone  = '' if $clone eq 'NULL';
+        $clone  = '' if $clone eq 'NULL';
 
         my $strand = $tile->{'strand'};
         $rstart     = 1 if $rstart < 1;
@@ -427,11 +427,13 @@ sub _init_non_assembled_contig {
             $glyph->{'zmenu'} = {
     		       'caption' => $rid,
     		       '02:Centre on contig' => $glyph->{'href'},
-    		       "03:EMBL source file" => 
-    		       $self->{'config'}->{'ext_url'}->get_url( 'EMBL', $clone)
     		};
+	    if ($clone){
+		$glyph->{'zmenu'}{"01:Clone: $clone"};
+		$glyph->{'zmenu'}{"03:EMBL source file"} = 
+    		       $self->{'config'}->{'ext_url'}->get_url( 'EMBL', $clone);
+	    }
         }
-        $glyph->{'zmenu'}{"01:Clone: $clone"} if $clone && $clone ne '';
     
         $self->push($glyph);
         $clone = $strand > 0 ? "$clone >" : "< $clone";
