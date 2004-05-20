1a62a056885805a433dea4442fb88ff6f9b97584
Web Admin <w3adm@sanger.ac.uk>
2004-05-20 08:31:44 +0000

    port changes / updates from HEAD rmp

--- modules/Bio/EnsEMBL/GlyphSet/decipher.pm
+++ modules/Bio/EnsEMBL/GlyphSet/decipher.pm
@@ -9,14 +9,19 @@
 #
 package Bio::EnsEMBL::GlyphSet::decipher;
 use strict;
-use vars qw(@ISA);
+use vars qw(@ISA $DECIPHER_COLOURS);
 use Bio::EnsEMBL::GlyphSet_simple;
 use Bio::EnsEMBL::SeqFeature;
 use EnsWeb;
 use Bio::Das;
 
 @ISA = qw(Bio::EnsEMBL::GlyphSet_simple);
-
+$DECIPHER_COLOURS = {
+		     'known_deletion'  => "b70000",
+		     'known_insertion' => "00b700",
+		     'novel_deletion'  => "f59787",
+		     'novel_insertion' => "87f587",
+		    };
 sub my_label { "DECIPHER"; }
 
 sub features {
@@ -27,6 +32,7 @@ sub features {
   my $start        = $slice->start();
   my $end          = $slice->end();
   my $chr          = $slice->seq_region_name();
+  my $cmap         = $self->{'config'}->colourmap();
   my $offset       = -$start+1;
   my $species_defs = &EnsWeb::species_defs();
   my $dbname       = EnsWeb::species_defs->ENSEMBL_TRACK_DAS_SOURCES->{"das_DECIPHER"};
@@ -34,6 +40,11 @@ sub features {
   $dsn             = "http://$dsn" unless ($dsn =~ /^https?:\/\//i);
   my @features     = ();
   my $das          = Bio::Das->new(20);
+
+  for my $k (keys %$DECIPHER_COLOURS) {
+    $cmap->add_hex($DECIPHER_COLOURS->{$k});
+  }
+
   $das->proxy($species_defs->ENSEMBL_DAS_PROXY);
 
   $das->features(
@@ -47,14 +58,20 @@ sub features {
 		);
 
   my $res   = [];
-  @features = sort { $a->orientation() cmp $b->orientation() || $a->group() cmp $b->group() } @features;
+  @features = sort {
+    $a->orientation() cmp $b->orientation() ||
+    $a->group() cmp $b->group()
+  } @features;
 
-  while(@features) {
-    my @parts  = $features[0..1];
-    my ($hard) = grep { $_->{'type'} !~ /fuzzy/ } @features;
-    my ($soft) = grep { $_->{'type'} =~ /fuzzy/ } @features;
-    shift @features;
-    shift @features;
+  my @novelfeatures = grep { $_->{'type'} =~ /novel/ } @features;
+  my @knownfeatures = grep { $_->{'type'} =~ /known/ } @features;
+
+  while(@novelfeatures) {
+    my @parts  = @novelfeatures[0..1];
+    my ($hard) = grep { $_->{'type'} !~ /fuzzy/ } @parts;
+    my ($soft) = grep { $_->{'type'} =~ /fuzzy/ } @parts;
+    shift @novelfeatures;
+    shift @novelfeatures;
 
     my $s = Bio::EnsEMBL::SeqFeature->new(
 					  -start   => $hard->start()+$offset,
@@ -64,10 +81,26 @@ sub features {
 					 );
     $s->{'decipher_softstart'} = $soft->start()+$offset;
     $s->{'decipher_softend'}   = $soft->end()+$offset;
-    $s->{'decipher_note'}      = $soft->note();
-    $s->{'decipher_link'}      = $soft->link();
+    $s->{'decipher_note'}      = $soft->note() || "";
+    $s->{'decipher_link'}      = $soft->link() || "";
     $s->{'decipher_strand'}    = ($soft->orientation() eq "-")?-1:1;
+    $s->{'decipher_thang'}     = "novel";
 
+    push @{$res}, $s;
+  }
+
+  for my $known (@knownfeatures) {
+    my $s = Bio::EnsEMBL::SeqFeature->new(
+					  -start   => $known->start()+$offset,
+					  -end     => $known->end()+$offset,
+					  -strand  => -1,
+					  -seqname => $known->label(),
+					 );
+    $s->{'decipher_type'}   = $known->type();
+    $s->{'decipher_note'}   = $known->note() || "";
+    $s->{'decipher_link'}   = $known->link() || "";
+    $s->{'decipher_strand'} = ($known->orientation() eq "-")?-1:1;
+    $s->{'decipher_thang'}  = "known";
     push @{$res}, $s;
   }
   return $res;
@@ -75,17 +108,25 @@ sub features {
 
 sub colour {
   my ($self, $f) = @_;
-  ($f->{'decipher_strand'} < 0)?"red":"green";
+  if(($f->{'decipher_thang'}||"") eq "known") {
+    return $DECIPHER_COLOURS->{($f->{'decipher_strand'} < 0)?'known_deletion':'known_insertion'};
+  }
+  return $DECIPHER_COLOURS->{($f->{'decipher_strand'} < 0)?'novel_deletion':'novel_insertion'};
 }
 
-sub href  { return undef; }
+sub href  {
+  my ($self, $f) = @_;
+  return $f->{'decipher_link'} || undef;
+}
+
 sub zmenu {
   my ($self, $f) = @_;
-  return {
-	  'caption' => "DECIPHER:" . $f->seqname(),
-	  '01:<b>Phenotype:</b>' . $f->{'decipher_note'} => undef,
-	  '02:Report' => $f->{'decipher_link'},
-	 };
+  my $ref = {
+	     'caption'   => "DECIPHER:" . $f->seqname(),
+	    };
+  $ref->{'01:<b>Phenotype:</b>' . $f->{'decipher_note'}} = undef if($f->{'decipher_note'});
+  $ref->{'02:Report'} = $f->{'decipher_link'} if($f->{'decipher_link'});
+  return $ref;
 }
 
 sub image_label {
@@ -95,6 +136,7 @@ sub image_label {
 
 sub tag {
   my ($self, $f) = @_;
+  return unless($f->{'decipher_softstart'} && $f->{'decipher_softend'});
   return ({
 	   'start'  => $f->{'decipher_softstart'},
 	   'end'    => $f->start(),
