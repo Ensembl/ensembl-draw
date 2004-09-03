5af4003ce74608ef9eacd40bd44f58f2a4c2ebb1
Patrick Meidl <pm2@sanger.ac.uk>
2004-09-03 15:02:30 +0000

    fixed Glovar::SNP->AUTOLOAD to allow unsetting attributes

--- modules/Bio/EnsEMBL/GlyphSet/glovar_snp.pm
+++ modules/Bio/EnsEMBL/GlyphSet/glovar_snp.pm
@@ -73,8 +73,8 @@ sub features {
     ## hack to disable consequences on chr7
     if ($self->{'container'}->seq_region_name == 7) {
         foreach my $snp (@snps) {
-            $snp->type("05:");
-            $snp->consequence("05:");
+            $snp->type(undef);
+            $snp->consequence(undef);
         }
     }
 
