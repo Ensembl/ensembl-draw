7d1db2961627d0662a6cae28e389e6783bca8976
Web Admin <w3adm@sanger.ac.uk>
2004-10-22 09:09:57 +0000

    freeze thaw stuff ... and configuration issues

--- modules/Bio/EnsEMBL/GlyphSet/generic_microarray.pm
+++ modules/Bio/EnsEMBL/GlyphSet/generic_microarray.pm
@@ -8,12 +8,12 @@ sub my_label {
   my $self = shift;
   my $key = '_my_label';
   if( ! exists( $self->{$key} ) ){
-    my $misc_set_code = $self->my_config('FEATURES') || 
-      die( "The FEATURES key of UserConfig must be a misc_set_code" );
+    my $misc_set_code = $self->my_config('FEATURES'); # || die( "The FEATURES key of UserConfig must be a misc_set_code" );
+    return undef unless $misc_set_code;
     my $db_adaptor = $self->{'container'}->adaptor->db;
     my $msa_adaptor = $db_adaptor->get_MiscSetAdaptor;
-    my $misc_set = $msa_adaptor->fetch_by_code( $misc_set_code ) ||
-      die( "The misc_set_code $misc_set_code is not found in the DB" );
+    my $misc_set = $msa_adaptor->fetch_by_code( $misc_set_code );
+    return undef unless $misc_set;
     $self->{$key} = $misc_set->name;
   }
   return $self->{$key};
@@ -24,8 +24,8 @@ sub my_label {
 
 sub features {
     my ($self) = @_;
-    my $misc_set_code = $self->my_config('FEATURES') || 
-      die( "The FEATURES key of UserConfig must be a misc_set_code" );
+    my $misc_set_code = $self->my_config('FEATURES');
+    return [] unless $misc_set_code;
     return $self->{'container'}->get_all_MiscFeatures( $misc_set_code );
 }
 
