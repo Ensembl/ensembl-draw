6b06fcabfcfde769a94686de1c08e2901dc555fa
Fiona Cunningham <fiona@ebi.ac.uk>
2004-10-25 15:07:35 +0000

    improved ambig code stuff

--- modules/Bio/EnsEMBL/GlyphSet/variation.pm
+++ modules/Bio/EnsEMBL/GlyphSet/variation.pm
@@ -105,6 +105,7 @@ sub zmenu {
  	       '01:SNP properties'   => $self->href( $f ),
  	       "02:bp: $pos"         => '',
  	       "03:status: ".($status || '-') => '',
+ 	       "03:variation type: ".($f->var_class || '-') => '',
  	       "07:ambiguity code: ".$f->ambig_code => '',
  	       "08:alleles: ".$f->allele_string => '',
 	      );
--- modules/Bio/EnsEMBL/GlyphSet/variation_box.pm
+++ modules/Bio/EnsEMBL/GlyphSet/variation_box.pm
@@ -6,19 +6,23 @@ use Bio::EnsEMBL::GlyphSet::variation;
 @ISA = qw(Bio::EnsEMBL::GlyphSet::variation);
 
 sub tag {
-    my ($self, $f) = @_; 
-    my ($col, $labcol) =  $self->colour($f);
-    #warn( "snp - $col - $labcol" );
-   # if($f->snpclass eq 'snp' ) {
-	return( { 'style' => 'box', 'letter' => $f->ambig_code, 'colour' => $col, 'label_colour' => $labcol } );
-   # }
-   # if($f->{'_range_type'} eq 'between' ) {
-   #	return( { 'style' => 'left-snp', 'colour' => $col } );
-   # }
-   # if($f->snpclass eq 'in-del' ) {
-   #	return( { 'style' => 'delta', 'colour' => $col } );
-   # }
-    return ( { 'style'  => 'box', 'colour' => $col, 'letter' => ' ' } );
+  my ($self, $f) = @_; 
+  my ($col, $labcol) =  $self->colour($f);
+  #warn( "snp - $col - $labcol" );
+
+  if($f->var_class eq 'snp' ) {
+    return( { 'style' => 'box', 'letter' => $f->ambig_code, 
+	      'colour' => $col, 'label_colour' => $labcol } );
+  }
+
+  if($f->{'_range_type'} eq 'between' ) {
+    return( { 'style' => 'left-snp', 'colour' => $col } );
+  }
+
+  if($f->var_class eq 'in-del' ) {
+    return( { 'style' => 'delta', 'colour' => $col } );
+  }
+  return ( { 'style'  => 'box', 'colour' => $col, 'letter' => ' ' } );
 }
 
 
