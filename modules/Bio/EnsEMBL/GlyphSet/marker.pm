6f5c012dc16f29ab9e894a84abe4f33aaaed999f
Steve Trevanion <st3@sanger.ac.uk>
2004-12-08 18:01:36 +0000

    fixes missing label

--- modules/Bio/EnsEMBL/GlyphSet/marker.pm
+++ modules/Bio/EnsEMBL/GlyphSet/marker.pm
@@ -11,11 +11,26 @@ use Sanger::Graphics::Bump;
 my $MAP_WEIGHT = 2;
 my $PRIORITY   = 50;
 
+sub init_label {
+  my ($self) = @_;
+  return if( defined $self->{'config'}->{'_no_label'} );
+  $self->label( new Sanger::Graphics::Glyph::Text({
+    'text'    => "Markers",
+    'font'    => 'Small',
+    'absolutey' => 1,
+    'href'    => qq[javascript:X=hw('@{[$self->{container}{_config_file_name_}]}','$ENV{'ENSEMBL_SCRIPT'}','markers')],
+    'zmenu'   => {
+    'caption'           => 'HELP',
+    "01:Track information..."   => qq[javascript:X=hw(\'@{[$self->{container}{_config_file_name_}]}\',\'$ENV{'ENSEMBL_SCRIPT'}\',\'markers\')]
+  }}));
+}
+
 sub _init {
   my $self = shift;
 
   my $slice         = $self->{'container'};
   my $Config        = $self->{'config'};
+  my $L             = $slice->length();
   my $pix_per_bp    = $Config->transform->{'scalex'};
   my $bitmap_length = int($slice->length() * $pix_per_bp);
   my @bitmap;
@@ -29,9 +44,10 @@ sub _init {
       $w             = $Config->texthelper->width($fontname);
 
   my $labels         = $Config->get('marker', 'labels' ) eq 'on';
-  foreach my $f (@{$slice->get_all_MarkerFeatures(undef,
-    $self->{container}{_config_file_name_} eq 'Homo_sapiens' ? $PRIORITY : 0,
-    $MAP_WEIGHT)}
+
+  my $priority = ($self->{container}{_config_file_name_} eq 'Homo_sapiens' ? 
+                  $PRIORITY : undef);
+  foreach my $f (@{$slice->get_all_MarkerFeatures(undef,$priority,$MAP_WEIGHT)}
   ){
     my $ms           = $f->marker->display_MarkerSynonym;
     my $fid          = $ms->name;
@@ -39,14 +55,16 @@ sub _init {
     my ($feature_colour, $label_colour, $part_to_colour) = $self->colour($f);
     my $href         = "/@{[$self->{container}{_config_file_name_}]}/markerview?marker=$fid";
 
+    my $S = $f->start()-1; $S = 0 if $S<0;
+    my $E = $f->end()    ; $E = $L if $E>$L;
     $self->push( new Sanger::Graphics::Glyph::Rect({
-      'x' => $f->start - 1,        'y' => 0,         'height' => $row_height, 'width' => ($f->end-$f->start+1),
+      'x' => $S,        'y' => 0,         'height' => $row_height, 'width' => ($E-$S+1),
       'colour' => $feature_colour, 'absolutey' => 1,
       'href'   => $href, 'zmenu' => { 'caption' => ($ms->source eq 'unists' ? "uniSTS:$fid" : $fid), 'Marker info' => $href }
     }));
     next unless $labels;
     my $glyph = new Sanger::Graphics::Glyph::Text({
-      'x'         => $f->start()-1,
+      'x'         => $S,
       'y'         => $row_height + 2,
       'height'    => $Config->texthelper->height($fontname),
       'width'     => $bp_textwidth,
