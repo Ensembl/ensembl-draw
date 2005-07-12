00ddbfa2ac3f28ecb00ffb2b4b6354b06ce21970
Steve Trevanion <st3@sanger.ac.uk>
2005-07-12 14:09:12 +0000

    fixed reading of depth from config

--- modules/Bio/EnsEMBL/GlyphSet/das.pm
+++ modules/Bio/EnsEMBL/GlyphSet/das.pm
@@ -56,6 +56,7 @@ else {
 # Render ungrouped features
 
 sub RENDER_simple {
+
   my( $self, $configuration ) = @_;
   my $empty_flag = 1;
 
@@ -685,7 +686,7 @@ sub _init {
   my @das_features = ();
 
   $configuration->{colour} = $Config->get($das_config_key, 'col') || $Extra->{color} || 'contigblue1';
-  $configuration->{depth} =  defined($Config->get($das_config_key, 'dep')) ? $Config->get($das_config_key, 'dep') : 4;
+  $configuration->{depth} =  defined($Config->get($das_config_key, 'dep')) ? $Config->get($das_config_key, 'dep') : $Extra->{depth} || 4;
   $configuration->{use_style} = $Extra->{stylesheet} ? uc($Extra->{stylesheet}) eq 'Y' : uc($Config->get($das_config_key, 'stylesheet')) eq 'Y';
   $configuration->{labelling} = $Extra->{labelflag} =~ /^[ou]$/i ? 1 : 0;
   $configuration->{length} = $container_length;
