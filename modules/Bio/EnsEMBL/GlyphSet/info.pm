bf58bd86d5ce0a6dbf021f889112944fae73b31c
Steve Trevanion <st3@sanger.ac.uk>
2006-03-02 18:28:43 +0000

    info track shows sitetype, not just Ensembl

--- modules/Bio/EnsEMBL/GlyphSet/info.pm
+++ modules/Bio/EnsEMBL/GlyphSet/info.pm
@@ -13,13 +13,15 @@ sub _init {
     return unless ($self->strand() == -1);
     my ($w,$h)   = $self->{'config'}->texthelper()->real_px2bp($self->{'config'}->species_defs->ENSEMBL_STYLE->{'LABEL_FONT'});
     my ($w2,$h2) = $self->{'config'}->texthelper()->real_px2bp('Small');
+	my $sitetype = ucfirst(lc($self->{'config'}->species_defs->ENSEMBL_SITETYPE));
     $self->push( new Sanger::Graphics::Glyph::Text({
         'x'         => 1, 
         'y'         => int( ($h2-$h)/2 ),
         'height'    => $h2,
         'font'      => $self->{'config'}->species_defs->ENSEMBL_STYLE->{'LABEL_FONT'},
         'colour'    => 'black',
-        'text'      => sprintf( "Ensembl %s    %s:%d-%d    %s",
+        'text'      => sprintf( "%s %s    %s:%d-%d    %s",
+		   $sitetype,
            @{[$self->{container}{_config_file_name_}]}, $self->{'container'}->seq_region_name,
            $self->{'container'}->start(), $self->{'container'}->end,
            scalar( gmtime() )
