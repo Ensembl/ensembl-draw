d1591e5693fa5a4c0da062eb0e72ce9f0f481053
Steve Trevanion <st3@sanger.ac.uk>
2007-05-14 11:28:19 +0000

    merge from HEAD

--- modules/Bio/EnsEMBL/GlyphSet/missing.pm
+++ modules/Bio/EnsEMBL/GlyphSet/missing.pm
@@ -2,6 +2,8 @@ package Bio::EnsEMBL::GlyphSet::missing;
 use strict;
 use vars qw(@ISA);
 use Bio::EnsEMBL::GlyphSet;
+
+use warnings;
 @ISA = qw(Bio::EnsEMBL::GlyphSet);
 
 sub init_label {
@@ -11,12 +13,13 @@ sub init_label {
 sub _init {
   my ($self) = @_;
   return unless ($self->strand() == -1);
+  return unless exists( $self->{'config'}->{'missing_tracks'} );
   my $tracks   = $self->{'config'}->{'missing_tracks'};
-
   my $Config        = $self->{'config'};
   my( $FONT,$FONTSIZE)  = $self->get_font_details( 'text' );
 
-  my $text_to_display = "All tracks are currently switched on";
+  #my $text_to_display = "All tracks are currently switched on";
+  my $text_to_display= "";  # temporary measure to stop incorrect info on browser until fixed properly
 
   if( $tracks > 1 ) {
     $text_to_display =  "There are currently $tracks tracks switched off, use the menus above the image to turn them on." ;
